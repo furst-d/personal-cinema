61508060176b20e9bb94741e947166f46f76f28a
Dominik Furst <dominikfurst1@seznam.cz>
2024-05-26 19:52:05 +0200

    Added PostgreSQL DB

--- backend/symfony/src/Controller/DefaultController.php
+++ backend/symfony/src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,23 @@ use Symfony\Component\Routing\Attribute\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): JsonResponse
+    public function index(EntityManagerInterface $em): JsonResponse
     {
+        try {
+            $connection = $em->getConnection();
+            $connection->connect();
+
+            if ($connection->isConnected()) {
+                $message = 'Database connection is successful!';
+            } else {
+                $message = 'Failed to connect to the database.';
+            }
+        } catch (Exception $e) {
+            $message = 'Error: ' . $e->getMessage();
+        }
+
         return $this->json([
-            'message' => 'Welcome to your new controller!',
+            'message' => $message,
             'path' => 'src/Controller/DefaultController.php',
         ]);
     }
