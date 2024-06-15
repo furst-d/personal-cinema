7e1f9f06c5fdb3cd4f884baa691b3ae6b0bb8580
Dominik Fűrst <dominikfurst1@seznam.cz>
2024-06-15 09:30:59 +0200

    Test commit

--- backend/symfony/src/Controller/DefaultController.php
+++ backend/symfony/src/Controller/DefaultController.php
@@ -14,7 +14,7 @@ class DefaultController extends ApiController
 //        $tests = $repository->findAll();
 //        $test = $repository->find(1);
         $payload = [
-            'message' => 'Welcome to your new controller!',
+            'message' => 'Welcome to your new controller! Test 2',
             'path' => 'src/Controller/DefaultController.php',
         ];
         return $this->re->withData($payload);
