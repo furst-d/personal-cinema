e0edaef50bb564b919e4e869d7e99b9c0d081e10
Dominik Fűrst <dominikfurst1@seznam.cz>
2024-06-28 15:43:16 +0200

    Set env vars

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,9 +1,11 @@
 function App() {
+    const apiUrl = import.meta.env.VITE_API_URL;
 
   return (
-    <>
-        <p>Test 1</p>
-    </>
+      <>
+          <p>Test 1</p>
+          <p>API URL: {apiUrl}</p>
+      </>
   )
 }
 
