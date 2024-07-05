262ee2154355db7c43f5e3a6e273f6925892799b
Dominik Fűrst <dominikfurst1@seznam.cz>
2024-07-05 16:21:50 +0200

    Remove test repository

--- backend/symfony/src/Controller/DefaultController.php
+++ backend/symfony/src/Controller/DefaultController.php
@@ -2,19 +2,16 @@
 
 namespace App\Controller;
 
-use App\Repository\TestRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DefaultController extends ApiController
 {
     #[Route('/', name: 'homepage')]
-    public function index(TestRepository $repository): JsonResponse
+    public function index(): JsonResponse
     {
-//        $tests = $repository->findAll();
-//        $test = $repository->find(1);
         $payload = [
-            'message' => 'Welcome to your new controller! Test 6',
+            'message' => 'Welcome to your new controller! Test 1',
             'path' => 'src/Controller/DefaultController.php',
         ];
         return $this->re->withData($payload);
