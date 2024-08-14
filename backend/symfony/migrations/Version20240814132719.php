bcb2febbacca77d760df6114796fb048f43c7e11
Dominik Fűrst <dominikfurst1@seznam.cz>
2024-08-14 15:29:51 +0200

    Added normalizer folder name

---
+++ backend/symfony/migrations/Version20240814132719.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240814132719 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE folder ADD normalized_name VARCHAR(255) NOT NULL DEFAULT \'\'');
+        $this->addSql('ALTER TABLE video ALTER normalized_name DROP DEFAULT');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE video ALTER normalized_name SET DEFAULT \'\'');
+        $this->addSql('ALTER TABLE folder DROP normalized_name');
+    }
+}
