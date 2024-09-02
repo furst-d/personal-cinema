3db8c2d4162b7c01b73ccdbbc8cedc36df0602b7
Dominik Fűrst <dominikfurst1@seznam.cz>
2024-09-02 14:12:31 +0200

    CDN routes documentation

--- backend/symfony/src/Controller/V1/Private/Cdn/CdnController.php
+++ backend/symfony/src/Controller/V1/Private/Cdn/CdnController.php
@@ -2,13 +2,27 @@
 
 namespace App\Controller\V1\Private\Cdn;
 
+use App\Attribute\OpenApi\Request\RequestBody;
+use App\Attribute\OpenApi\Response\ResponseData;
+use App\Attribute\OpenApi\Response\ResponseError;
+use App\Attribute\OpenApi\Response\ResponseMessage;
 use App\Controller\V1\Private\BasePrivateController;
 use App\DTO\Video\CdnNotificationRequest;
 use App\DTO\Video\CdnThumbnailRequest;
+use App\DTO\Video\FolderRequest;
+use App\Entity\Video\Folder;
 use App\Exception\ApiException;
+use App\Exception\BadRequestException;
+use App\Exception\InternalException;
+use App\Exception\NotFoundException;
+use App\Exception\UnauthorizedException;
 use App\Service\Auth\AuthService;
 use App\Service\Cdn\CdnService;
 use App\Service\Locator\BaseControllerLocator;
+use App\Service\Video\FolderService;
+use App\Service\Video\VideoService;
+use OpenApi\Attributes as OA;
+use Nelmio\ApiDocBundle\Annotation\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
@@ -20,6 +34,8 @@ class CdnController extends BasePrivateController
      * @var CdnService $cdnService
      */
     private CdnService $cdnService;
+
+    public const TAG = 'private/cdn';
 
     /**
      * @param BaseControllerLocator $locator
@@ -36,6 +52,18 @@ class CdnController extends BasePrivateController
         $this->cdnService = $cdnService;
     }
 
+    #[OA\Post(
+        description: "Is used by CDN to notify about changes in video, e.g. conversions generated.",
+        summary: "Process notification from CDN",
+        requestBody: new RequestBody(entityClass: CdnNotificationRequest::class),
+        tags: [self::TAG],
+    )]
+    #[ResponseMessage(message: 'Notification processed.')]
+    #[ResponseError(exception: new BadRequestException())]
+    #[ResponseError(exception: new UnauthorizedException())]
+    #[ResponseError(exception: new NotFoundException(VideoService::NOT_FOUND_MESSAGE))]
+    #[ResponseError(exception: new InternalException())]
+    #[Security(name: 'Bearer')]
     #[Route('/notification', name: 'cdn_notification', methods: ['POST'])]
     public function actionNotification(Request $request, CdnNotificationRequest $notificationRequest): JsonResponse
     {
@@ -48,6 +76,18 @@ class CdnController extends BasePrivateController
         }
     }
 
+    #[OA\Post(
+        description: "Is used by CDN to notify about generated thumbnails.",
+        summary: "Process thumbnails from CDN",
+        requestBody: new RequestBody(entityClass: CdnThumbnailRequest::class),
+        tags: [self::TAG],
+    )]
+    #[ResponseMessage(message: 'Thumb processed.')]
+    #[ResponseError(exception: new BadRequestException())]
+    #[ResponseError(exception: new UnauthorizedException())]
+    #[ResponseError(exception: new NotFoundException(VideoService::NOT_FOUND_MESSAGE))]
+    #[ResponseError(exception: new InternalException())]
+    #[Security(name: 'Bearer')]
     #[Route('/thumb', name: 'cdn_thumb', methods: ['POST'])]
     public function actionThumb(Request $request, CdnThumbnailRequest $thumbnailRequest): JsonResponse
     {
--- backend/symfony/src/DTO/Video/CdnThumbnailRequest.php
+++ backend/symfony/src/DTO/Video/CdnThumbnailRequest.php
@@ -4,13 +4,16 @@ namespace App\DTO\Video;
 
 use App\DTO\AbstractRequest;
 use Symfony\Component\Validator\Constraints as Assert;
+use OpenApi\Attributes as OA;
 
 class CdnThumbnailRequest extends AbstractRequest
 {
     #[Assert\NotBlank]
     #[Assert\Valid]
+    #[OA\Property(description: "Video for which thumbnails are generated")]
     public CdnVideoRequest $video;
 
     #[Assert\NotBlank]
+    #[OA\Property(description: "Thumbnail links generated for the video")]
     public array $thumbs;
 }
