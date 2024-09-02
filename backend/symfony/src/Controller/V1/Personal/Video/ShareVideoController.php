c4b00feb6a44975978d14906a96883c847c0d0e8
Dominik Fűrst <dominikfurst1@seznam.cz>
2024-09-02 11:36:07 +0200

    Videos routes documentation

--- backend/symfony/src/Controller/V1/Personal/Video/ShareVideoController.php
+++ backend/symfony/src/Controller/V1/Personal/Video/ShareVideoController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller\V1\Personal\Video;
 
+use App\Attribute\OpenApi\Request\RequestBody;
+use App\Attribute\OpenApi\Response\ResponseData;
+use App\Attribute\OpenApi\Response\ResponseError;
+use App\Attribute\OpenApi\Response\ResponseMessage;
 use App\Controller\V1\Personal\BasePersonalController;
 use App\DTO\Account\TokenRequest;
 use App\DTO\Video\VideoPublicShareRequest;
@@ -10,20 +14,27 @@ use App\DTO\Video\VideoShareRequest;
 use App\Entity\Video\Share\ShareVideoPublic;
 use App\Entity\Video\Video;
 use App\Exception\ApiException;
+use App\Exception\BadGatewayException;
 use App\Exception\BadRequestException;
+use App\Exception\ConflictException;
+use App\Exception\ForbiddenException;
+use App\Exception\InternalException;
+use App\Exception\NotFoundException;
+use App\Exception\UnauthorizedException;
 use App\Helper\Jwt\JwtUsage;
 use App\Helper\Regex\RegexRoute;
-use App\Helper\Video\FolderData;
 use App\Service\Account\AccountService;
 use App\Service\Jwt\JwtService;
 use App\Service\Locator\BaseControllerLocator;
 use App\Service\Mailer\MailerService;
-use App\Service\Video\FolderService;
 use App\Service\Video\ShareService;
 use App\Service\Video\VideoService;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryString;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/personal/videos/share')]
@@ -79,8 +90,18 @@ class ShareVideoController extends BasePersonalController
         $this->mailerService = $mailerService;
     }
 
+    #[OA\Get(
+        description: "Retrieve a list of videos that was shared with the user",
+        summary: "List of videos shared with the user",
+        tags: [VideoController::TAG],
+    )]
+    #[ResponseData(entityClass: Video::class, groups: [Video::VIDEOS_READ], pagination: true, description: "List of videos")]
+    #[ResponseError(exception: new BadRequestException())]
+    #[ResponseError(exception: new UnauthorizedException())]
+    #[ResponseError(exception: new InternalException())]
+    #[Security(name: 'Bearer')]
     #[Route('', name: 'user_shared_videos', methods: ['GET'])]
-    public function getSharedVideos(Request $request, VideoQueryRequest $videoQueryRequest): JsonResponse
+    public function getSharedVideos(Request $request, #[MapQueryString] VideoQueryRequest $videoQueryRequest): JsonResponse
     {
         try {
             $account = $this->getAccount($request);
@@ -98,6 +119,21 @@ class ShareVideoController extends BasePersonalController
         }
     }
 
+    #[OA\Post(
+        description: "Share a video with another user",
+        summary: "Share a video",
+        requestBody: new RequestBody(entityClass: VideoShareRequest::class),
+        tags: [VideoController::TAG],
+    )]
+    #[ResponseMessage(message: "Video share request was send to the target email")]
+    #[ResponseError(exception: new BadRequestException())]
+    #[ResponseError(exception: new UnauthorizedException())]
+    #[ResponseError(exception: new ForbiddenException(ShareService::NO_SHARE_VIDEO_WITH_YOURSELF_MESSAGE))]
+    #[ResponseError(exception: new NotFoundException(ShareService::VIDEO_NOT_FOUND_MESSAGE))]
+    #[ResponseError(exception: new ConflictException(ShareService::VIDEO_ALREADY_SHARED_MESSAGE))]
+    #[ResponseError(exception: new InternalException())]
+    #[ResponseError(exception: new BadGatewayException("Failed to send email"))]
+    #[Security(name: 'Bearer')]
     #[Route('', name: 'user_share_video', methods: ['POST'])]
     public function share(Request $request, VideoShareRequest $shareRequest): JsonResponse
     {
@@ -126,6 +162,19 @@ class ShareVideoController extends BasePersonalController
         }
     }
 
+    #[OA\Post(
+        description: "Accept a video share request",
+        summary: "Accept a video share",
+        requestBody: new RequestBody(entityClass: TokenRequest::class),
+        tags: [VideoController::TAG],
+    )]
+    #[ResponseData(entityClass: Video::class, groups: [Video::VIDEOS_READ], description: "Shared video")]
+    #[ResponseError(exception: new BadRequestException())]
+    #[ResponseError(exception: new UnauthorizedException())]
+    #[ResponseError(exception: new NotFoundException())]
+    #[ResponseError(exception: new ConflictException(ShareService::VIDEO_ALREADY_SHARED_MESSAGE))]
+    #[ResponseError(exception: new InternalException())]
+    #[Security(name: 'Bearer')]
     #[Route('/accept', name: 'user_accept_share_video', methods: ['POST'])]
     public function acceptShare(Request $request, TokenRequest $tokenRequest): JsonResponse
     {
@@ -146,6 +195,19 @@ class ShareVideoController extends BasePersonalController
         }
     }
 
+    #[OA\Post(
+        description: "Create a public link to share a video",
+        summary: "Create a public link to share a video",
+        requestBody: new RequestBody(entityClass: VideoPublicShareRequest::class),
+        tags: [VideoController::TAG],
+    )]
+    #[ResponseData(entityClass: ShareVideoPublic::class, groups: [ShareVideoPublic::VIDEO_SHARED_PUBLIC_READ], description: "Shared video public link")]
+    #[ResponseError(exception: new BadRequestException())]
+    #[ResponseError(exception: new UnauthorizedException())]
+    #[ResponseError(exception: new ForbiddenException(ShareService::CANNOT_CREATE_LINK_MESSAGE))]
+    #[ResponseError(exception: new NotFoundException(VideoService::NOT_FOUND_MESSAGE))]
+    #[ResponseError(exception: new InternalException())]
+    #[Security(name: 'Bearer')]
     #[Route('/public', name: 'user_create_public_link', methods: ['POST'])]
     public function createPublicLink(Request $request, VideoPublicShareRequest $shareRequest): JsonResponse
     {
@@ -161,6 +223,16 @@ class ShareVideoController extends BasePersonalController
         }
     }
 
+    #[OA\Delete(
+        description: "Delete a video share.",
+        summary: "Delete a video share",
+        tags: [VideoController::TAG],
+    )]
+    #[ResponseMessage(message: "Video share deleted.")]
+    #[ResponseError(exception: new UnauthorizedException())]
+    #[ResponseError(exception: new NotFoundException(ShareService::VIDEO_SHARE_NOT_FOUND_MESSAGE))]
+    #[ResponseError(exception: new InternalException())]
+    #[Security(name: 'Bearer')]
     #[Route(RegexRoute::ID, name: 'user_delete_video_share', methods: ['DELETE'])]
     public function deleteVideoShare(Request $request, int $id): JsonResponse
     {
