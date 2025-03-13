21a9b5045324f977b52ec099891d8d0fae4d9a57
Dominik Fűrst <dominikfurst1@seznam.cz>
2025-03-13 12:46:19 +0100

    Update init db script

--- cdn-backend/scripts/init-db.sql
+++ cdn-backend/scripts/init-db.sql
@@ -1,14 +1,14 @@
 -- Insert data into Callbacks table
-INSERT INTO Callbacks (notificationUrl, thumbUrl)
+INSERT INTO callbacks (notificationUrl, thumbUrl)
 VALUES
-    ('http://172.26.86.206:8080/v1/private/cdn/notification?token=testNotificationToken', 'http://172.26.86.206:8080/v1/private/cdn/thumb?token=testThumbToken');
+    ('http://localhost:8080/v1/private/cdn/notification?token=testNotificationToken', 'http://localhost/v1/private/cdn/thumb?token=testThumbToken');
 
 -- Insert data into Projects table with test API key
-INSERT INTO Projects (name, apiKey, callbackId)
+INSERT INTO projects (name, apiKey, callbackId)
 VALUES
     ('SoukromeKino', 'testApiKey12345', (SELECT id FROM Callbacks LIMIT 1));
 
 -- Insert data into Settings table
-INSERT INTO Settings (key, value)
+INSERT INTO settings (key, value)
 VALUES
     ('video_size_limit', '100MB');
